593c42a9b16cef2b82f23430885fcdf8837eea57
M Ahaduzzaman Chowdhury <ahaduzzaman.chd@gmail.com>
2026-07-25 00:11:05 +0600

    fixed database.py for deployment

--- core/database.py
+++ core/database.py
@@ -2,14 +2,16 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# It will look for the Koyeb environment variable first. 
-# If it fails, it defaults to your local database for testing.
 DATABASE_URL = os.getenv(
-    "DATABASE_URL", 
+    "DATABASE_URL",
     "postgresql+asyncpg://postgres:yourlocalpassword@localhost/the_feed"
 )
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+# Render and Neon require SSL, but your local machine does not.
+# We dynamically add the SSL argument only if connecting to the cloud.
+connect_args = {"ssl": "require"} if "render.com" in DATABASE_URL or "neon.tech" in DATABASE_URL else {}
+
+engine = create_async_engine(DATABASE_URL, echo=False, connect_args=connect_args)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
